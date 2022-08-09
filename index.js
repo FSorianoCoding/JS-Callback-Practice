afe3f68332a61c935aef74521d59f8876e188afe
Fabian Soriano <fabo.soriano@gmail.com>
2022-08-08 22:38:26 -0700

    completed activity instructions without bonus

--- index.js
+++ index.js
@@ -2,7 +2,83 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+// Creating and wrapping our 'character' direction in a function 
+// to pass as a callback in the .withArrowKeys function below so that it passes through the move code  
+
+function handleDirectionChange(direction){
+    //Adding code for character moving with different gifs
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+// Passing handleDirectionChange as call back into .withArrowKeys to handle moving code.
+move(character).withArrowKeys(100, 250, handleDirectionChange)  //changed from .to(100,250) to .withArrowKeys(100,250)
+
+// Adding code for direction change of greenCharacter
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+// setInterval (function(){
+//     if (direction === 'west'){
+//         x = x - 1
+//     }
+//     if(direction === 'north'){
+//         y = y + 1
+//     }
+//     if(direction === 'east'){
+//         x = x + 1
+//     }
+//     if(direction === 'south'){
+//         y = y - 1
+//     }
+//     character.style.left = x + 'px'
+//     character.style.bottom = y + 'px'
+// }, 1)
+
+
+//Function to get the character to move pressing arrow keys
+// document.addEventListener('keydown', function(e){
+
+//     // "e" is an object containing details about the event that fired.
+//     if(e.repeat) return;
+
+//     if(e.key === 'ArrowLeft'){
+//         direction = 'west'
+//     }
+//     if(e.key === 'ArrowUp'){
+//         direction = 'north'
+//     }
+//     if(e.key === 'ArrowRight'){
+//         direction = 'east'
+//     }
+//     if(e.key === 'ArrowDown'){
+//         direction = 'south'
+//     }
+// })
+
+//Function to get character to stop after releasing the keys
+// document.addEventListener('keyup', function(e){
+//     direction = null
+// })
+
+// setInterval(moveCharacter, 1)  
+
+//end direction code 
 
 
 move(newImage('assets/tree.png')).to(200, 450)
--- move.js
+++ move.js
@@ -6,7 +6,67 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+//Refractering key buttons into move function 
+    //adding handleDirection Change callback as a passthrough, add as function down in lines 56 and 63
+    function moveWithArrowKeys(left, bottom, handleDirectionChange){  
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+
+    function moveCharacter(){
+        if(direction === 'west'){
+            x-=1
+        }
+        if(direction === 'north'){
+            y+=1
+        }
+        if(direction === 'east'){
+            x+=1
+        }
+        if(direction === 'south'){
+            y-=1
+        }
+        element.style.left = x +'px'
+        element.style.bottom = y + 'px'
+    }
+
+    setInterval(moveCharacter, 1)
+
+    //Function to get the character to move pressing arrow keys
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        // adding callback where we change the direction variable from line 11
+        // adding "direction" as the passthrough argument to work with handleDirectionChange defined in the index
+        handleDirectionChange(direction)  
+    })
+
+    document.addEventListener('keyup', function(e){
+        direction = null
+        // adding callback where we stop changing direction variable from line 11
+        // adding "direction" as the passthrough argument to work with handleDirectionChange defined in the index
+        handleDirectionChange(direction)  
+    })
+
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
